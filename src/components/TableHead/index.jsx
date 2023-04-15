bb71602ebd063bd198628cbdbc48b2f32c1a4522
kiz0r <kkhomenko10@gmail.com>
2023-04-16 00:11:54 +0200

    add cursor:pointer into divs with dates

--- src/components/TableHead/index.jsx
+++ src/components/TableHead/index.jsx
@@ -1,14 +1,8 @@
-import { startOfWeek, format, addDays } from 'date-fns';
-
 import styles from './../Calendar/Calendar.module.sass';
 const { weekDay } = styles;
 
 const TableHead = () => {
-  // const weekStartDate = startOfWeek(activeDate);
   const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-  // for (let day = 0; day < 7; day++) {
-  //   weekDays.push(format(addDays(weekStartDate, day), 'EEEEE'));
-  // }
 
   return (
     <thead>
